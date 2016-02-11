e3df1a05dd36229c7d8e18778f98457df0ef9973
Rustam Agametov <agametov@gmail.com>
2016-02-11 19:26:40 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+#
+# Project: Video Streaming with face recognition
+# Author: agametov [at] gmail [dot] com>
+# Date: 2016/02/11
+# Website: http://www.agametov.ru/
+# Usage:
+# 1. Install Python dependencies: cv2, flask. (wish that pip install works like a charm)
+# 2. Run "python main.py".
+# 3. Navigate the browser to the local webpage.
+from flask import Flask, render_template, Response
+from camera import VideoCamera
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(VideoCamera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=False)
